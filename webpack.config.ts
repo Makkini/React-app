0abba4cc04c70776187b039fe87788e6a131869d
Makkinni <finmakmislgood@gmail.com>
2023-06-01 12:54:50 +0300

    dev-server

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,21 +1,23 @@
 import webpack from 'webpack';
-import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
-import {BuildPaths} from "./config/build/types/config";
-import path from "path";
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
+import { BuildEnv, BuildPaths } from './config/build/types/config';
+import path from 'path';
 
-const paths: BuildPaths = {
+export default (env: BuildEnv) => {
+  const paths: BuildPaths = {
     entry: path.resolve(__dirname, 'src', 'index.ts'),
     build: path.resolve(__dirname, 'build'),
     html: path.resolve(__dirname, 'public', 'index.html'),
-}
+  };
+  const PORT = env.port || 3000;
+  const mode = env.mode ||'development';
+  const isDev = mode === 'development';
 
-const mode = 'development';
-const isDev = mode === 'development';
-
-const config: webpack.Configuration = buildWebpackConfig({
-    mode: 'development',
+  const config: webpack.Configuration = buildWebpackConfig({
+    mode,
     paths,
     isDev,
-})
-
-export default config;
+    port: PORT,
+  });
+  return config;
+};
