e11f7b55f37ffec4a5dfe708140f45d2d26f78f9
Makkinni <finmakmislgood@gmail.com>
2023-06-03 11:32:20 +0300

    navbar - first ui component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,17 @@
-import { Suspense } from 'react';
-import { Link } from 'react-router-dom';
-import { Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
-
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
 import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={'/'}>Главная</Link>
-      <Link to={'/about'}>О сайте</Link>
+        <Navbar/>
         <AppRouter/>
+      <button onClick={toggleTheme}>TOGGLE</button>
     </div>
   );
 };
