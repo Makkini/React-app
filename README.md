53c2fffb6583685d95c41cc5695b563af3cd8795
Makkinni <finmakmislgood@gmail.com>
2023-06-04 13:26:13 +0300

    i18n

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # React-app
+npm install
+npm start
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import { useTranslation } from 'react-i18next';
+
 const MainPage = () => {
-  return <div>MainPage</div>;
+  const { t } = useTranslation();
+  return (
+
+    <div>
+        {t('Главная страница')}
+        {t('Новый перевод')}
+    </div>
+  );
 };
 export default MainPage;
