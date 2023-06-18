d9cbe29691dd36a533450f6aec7822df4326dc2a
Makkinni <finmakmislgood@gmail.com>
2023-06-18 09:40:18 +0300

    test loginByUsername, add TestAsyncThunk

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -27,7 +27,7 @@ export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, { re
       return response.data;
     } catch (e) {
       console.log(e);
-      return thunkAPI.rejectWithValue('Вы ввели неверный логин или пароль');
+      return thunkAPI.rejectWithValue('error');
     }
   },
 );
