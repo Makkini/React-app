7313047535a00e3728a6af7674de5e40da9f194f
Makkinni <finmakmislgood@gmail.com>
2023-06-27 17:20:25 +0300

    add comments list

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -1,43 +1,50 @@
-import { AnyAction, CombinedState, Dispatch, EnhancedStore, Reducer, ReducersMapObject } from "@reduxjs/toolkit";
-import { AxiosInstance } from "axios";
-import { ArticleDetailsSchema } from "entities/Article";
-import { CounterSchema } from "entities/Counter";
-import { ProfileSchema } from "entities/Profile";
-import { UserSchema } from "entities/User";
-import { LoginSchema } from "features/AuthByUsername";
-import { To, NavigateOptions } from "react-router-dom";
+import {
+    AnyAction,
+    CombinedState,
+    Dispatch,
+    EnhancedStore,
+    Reducer,
+    ReducersMapObject,
+} from '@reduxjs/toolkit';
+import { AxiosInstance } from 'axios';
+import { ArticleDetailsSchema } from 'entities/Article';
+import { CounterSchema } from 'entities/Counter';
+import { ProfileSchema } from 'entities/Profile';
+import { UserSchema } from 'entities/User';
+import { LoginSchema } from 'features/AuthByUsername';
+import { ArticleDetailsCommentsSchema } from 'pages/ArticleDetailsPage';
+import { To, NavigateOptions } from 'react-router-dom';
 
-
-export interface StateSchema{
+export interface StateSchema {
     counter: CounterSchema;
     user: UserSchema;
 
     //Async reducers
-    loginForm?: LoginSchema
-    profile?: ProfileSchema
-    articleDetails?: ArticleDetailsSchema
+    loginForm?: LoginSchema;
+    profile?: ProfileSchema;
+    articleDetails?: ArticleDetailsSchema;
+    articleDetailsComments?: ArticleDetailsCommentsSchema;
 }
 
 export type StateSchemaKey = keyof StateSchema;
 export interface ReducerManager {
-    getReducerMap: () =>  ReducersMapObject<StateSchema>
-    reduce: (state: StateSchema, action: AnyAction) =>  CombinedState<StateSchema>
-    add: (key: StateSchemaKey, reducer: Reducer) => void
-    remove: (key:StateSchemaKey) => void
+    getReducerMap: () => ReducersMapObject<StateSchema>;
+    reduce: (state: StateSchema, action: AnyAction) => CombinedState<StateSchema>;
+    add: (key: StateSchemaKey, reducer: Reducer) => void;
+    remove: (key: StateSchemaKey) => void;
 }
 
-export interface ReduxStoreWithManager extends EnhancedStore<StateSchema>  {
-
-    reducerManager: ReducerManager
+export interface ReduxStoreWithManager extends EnhancedStore<StateSchema> {
+    reducerManager: ReducerManager;
 }
 
 export interface ThunkExtraArg {
-    api: AxiosInstance
-    navigate?: (to: To, options?: NavigateOptions)=>void
+    api: AxiosInstance;
+    navigate?: (to: To, options?: NavigateOptions) => void;
 }
 
-export interface ThunkConfig<T>{
-    rejectValue: T
+export interface ThunkConfig<T> {
+    rejectValue: T;
     extra: ThunkExtraArg;
-    state: StateSchema
+    state: StateSchema;
 }
